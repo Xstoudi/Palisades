66254f9bddb7f77812d3588c8efa0efc4af3f542
Xavier Stouder <xavier@stouder.io>
2022-07-27 19:24:04 +0200

    feat: add sentry error reporting

--- Palisades.Application/App.xaml.cs
+++ Palisades.Application/App.xaml.cs
@@ -1,15 +1,40 @@
-﻿
+﻿using Sentry;
+using System.Windows.Threading;
+
 namespace Palisades
 {
     public partial class App : System.Windows.Application
     {
         public App()
         {
+
+            SetupSentry();
+
             PalisadesManager.LoadPalisades();
             if (PalisadesManager.palisades.Count == 0)
             {
                 PalisadesManager.CreatePalisade();
             }
+        }
+
+        private void SetupSentry()
+        {
+            DispatcherUnhandledException += App_DispatcherUnhandledException;
+
+            SentrySdk.Init(o =>
+            {
+                o.Dsn = "https://ffd9f3db270c4bd583ab3041d6264c38@o1336793.ingest.sentry.io/6605931";
+                o.Debug = true;
+                o.TracesSampleRate = 1;
+            });
+
+            SentrySdk.CaptureMessage("Hello Sentry");
+        }
+
+        void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            SentrySdk.CaptureException(e.Exception);
+            e.Handled = true;
         }
     }
 }
